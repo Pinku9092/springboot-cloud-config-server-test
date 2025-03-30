c08fab0df6e2a5f45037e02281caf17787ddde71
pinku <kpinku32@gmail.com>
2025-03-30 12:04:51 +0530

    first commit

---
+++ src/main/java/com/pinku/spring/boot/cloud/config/test/api/SpringbootCloudConfigServerTestApplication.java
@@ -0,0 +1,26 @@
+package com.pinku.spring.boot.cloud.config.test.api;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController("/api")
+public class SpringbootCloudConfigServerTestApplication {
+
+	@Value("${microservices.service.name}")
+	private String name;
+
+	@GetMapping("/show")
+	public String getName(){
+		return "your config server name is : "+name;
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCloudConfigServerTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pinku/spring/boot/cloud/config/test/api/SpringbootCloudConfigServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pinku.spring.boot.cloud.config.test.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCloudConfigServerTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
